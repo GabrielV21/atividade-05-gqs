2db23c98b266db682417e11b134f667641030d04
VictorjosephF <victorfariaj@gmail.com>
2025-05-22 14:45:04 -0300

    Fix:Test: correcao de classe

--- src/testSenhaComMenosDeDoisDigitos.java
+++ src/testSenhaComMenosDeDoisDigitos.java
@@ -1,9 +1,13 @@
 
-@Test
-void testSenhaComDoisOuMaisDigitos() {
-    ValidadorSenha senha = new ValidadorSenha();
-    senha.validaDigitos("Abcde12@"); // 2 dígitos
-    assertTrue(senha.isValid());
-    assertTrue(senha.getErros().isEmpty());
+
+public class doisDigitosTeste() {
+    @Test
+    void testSenhaComDoisOuMaisDigitos() {
+        ValidadorSenha senha = new ValidadorSenha();
+        senha.validaDigitos("Abcde12@"); // 2 dígitos
+        assertTrue(senha.isValid());
+        assertTrue(senha.getErros().isEmpty());
+    }
+
 }
 

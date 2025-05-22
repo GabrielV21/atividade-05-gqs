3aad177ca055f204bc076c62baa67f964771a821
GabrielV21 <gabrielvasconcelosv21@gmail.com>
2025-05-21 23:05:13 -0300

    feat: Classe inicial

---
+++ src/ValidadorSenha.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidadorSenha {
+
+    public boolean valid;
+    public List<String> errors = new ArrayList<>();
+
+    public boolean isValid() {
+        return valid;
+    }
+    public List<String> getErrors() {
+        return errors;
+    }
+    
+}
